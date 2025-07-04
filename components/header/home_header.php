227f9018decbcc10dfe3f7cf2f9786f7bc862883
Lenzko <faizizz0912@gmail.com>
2025-07-04 14:51:49 +0700

    first commit

---
+++ components/header/home_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container w-100 d-flex justify-content-between">
+      <div class="">
+        <a class="navbar-brand" href="<?= BASE_URL. '/' ?>">
+          <img src="http://smkpansa.sch.id/media_library/images/1468ab823038e7901c854e5445c8a664.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-center  ">
+          <STrong>PANSA</STrong>
+        </a>
+      </div>
+      <div class="">
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>">Beranda</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>about">Tentang</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>artikel">Kegiatan</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>galeri">Galeri</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
