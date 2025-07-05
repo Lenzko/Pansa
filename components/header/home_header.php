0d4deee6b21210b9d7705105fae8c18df8c0d96a
Lenzko <faizizz0912@gmail.com>
2025-07-05 10:07:51 +0700

    ini baru

--- components/header/home_header.php
+++ components/header/home_header.php
@@ -53,7 +53,7 @@
       <div class="">
         <ul class="navbar-nav justify-content-end nav-putih">
           <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>">Beranda</a></li>
-          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>about">Tentang</a></li>
+          <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>about">Profil</a></li>
           <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>artikel">Kegiatan</a></li>
           <li class="nav-item"><a class="nav-link" href="<?= BASE_URL ?>galeri">Galeri</a></li>
         </ul>
--- views/galeri/index.php
+++ views/galeri/index.php
@@ -1,14 +1,30 @@
-<div class="container py-4">
-  <div class="text-center justify-content-center align-items-center d-flex flex-column my-5">
-    <div class="w-50">
-      <h1>Galeri Kegiatan Kami</h1>
-    </div>
+
+  <div class="text-center justify-content-center align-items-center d-flex flex-column my-3">
+        <div class="w-50">
+          <h1>Galeri Kegiatan Kami</h1>
+        </div>
+      </div>
+  <div id="galeriCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+  <div class="carousel-inner">
+    <?php 
+      $active = true;
+      foreach ($data['galeri'] as $galeri) { ?>
+      <div class="carousel-item <?= $active ? 'active' : '' ?>">
+        <img src="<?= $galeri['image'] ?>" class="d-block mx-auto img-fluid rounded" alt="<?= $galeri['title'] ?>" style="max-height: 400px;">
+      </div>
+    <?php 
+      $active = false;
+      } 
+    ?>
   </div>
-  <div class="row">
-    <?php foreach($data['galeri'] as $galery) { ?> 
-    <div class="col-md-3">
-      <img src="<?= $galery['image'] ?>" alt="<?= $galery['title'] ?>" class="img-fluid rounded mb-3" width="280" height="300">
-    </div>
-    <?php } ?>
-  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#galeriCarousel" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#galeriCarousel" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </button>
+  <br>
 </div>
+
+
+
